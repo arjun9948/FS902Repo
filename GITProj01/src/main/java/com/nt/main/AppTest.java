5a47bd54e292817a2d2adf3cc658ffb947a7af74
Arjun Reddy <arjunreddya049@gmail.com>
2024-02-27 20:14:39 +0530

    task1 from dev1

--- GITProj01/src/main/java/com/nt/main/AppTest.java
+++ GITProj01/src/main/java/com/nt/main/AppTest.java
@@ -1,8 +1,13 @@
 package com.nt.main;
 
+import com.nt.service.ArithmeticOperations;
+
 public class AppTest {
 
 	public static void main(String[] args) {
 		
+		ArithmeticOperations ar = new ArithmeticOperations();
+		System.out.println("sum:"+ ar.sum(20, 40));
+		System.out.println("sub:"+ar.sub(20, 30));
 	}
 }
